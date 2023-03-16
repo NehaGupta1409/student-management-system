647a6e61e7513460181444d1f96b4a7c51cd5650
NehaGupta1409 <nehasathyendargupta14@gmail.com>
2023-03-16 20:54:02 +0530

    Fist commit

---
+++ src/data/index.js
@@ -0,0 +1,74 @@
+const studentsData = [
+    {
+      id: 1,
+      firstName: 'Susan',
+      lastName: 'Jordon',
+      email: 'susan@example.com',
+      date: '2019-04-11'
+    },
+    {
+      id: 2,
+      firstName: 'Adrienne',
+      lastName: 'Doak',
+      email: 'adrienne@example.com',
+      date: '2019-04-17'
+    },
+    {
+      id: 3,
+      firstName: 'Rolf',
+      lastName: 'Hegdal',
+      email: 'rolf@example.com',
+      date: '2019-05-01'
+    },
+    {
+      id: 4,
+      firstName: 'Kent',
+      lastName: 'Rosner',
+      email: 'kent@example.com',
+      date: '2019-05-03'
+    },
+    {
+      id: 5,
+      firstName: 'Arsenio',
+      lastName: 'Grant',
+      email: 'arsenio@example.com',
+      date: '2019-06-13'
+    },
+    {
+      id: 6,
+      firstName: 'Laurena',
+      lastName: 'Lurie',
+      email: 'laurena@example.com',
+      date: '2019-07-30'
+    },
+    {
+      id: 7,
+      firstName: 'George',
+      lastName: 'Tallman',
+      email: 'george@example.com',
+      date: '2019-08-15'
+    },
+    {
+      id: 8,
+      firstName: 'Jesica',
+      lastName: 'Watlington',
+      email: 'jesica@example.com',
+      date: '2019-10-10'
+    },
+    {
+      id: 9,
+      firstName: 'Matthew',
+      lastName: 'Warren',
+      email: 'matthew@example.com',
+      date: '2019-10-15'
+    },
+    {
+      id: 10,
+      firstName: 'Lyndsey',
+      lastName: 'Follette',
+      email: 'lyndsey@example.com',
+      date: '2020-01-15'
+    }
+  ];
+  
+  export { studentsData };
